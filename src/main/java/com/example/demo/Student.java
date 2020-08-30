31fbf099ac1cd5157918a662da9bcfb2e8c26933
rgbk21 <rajgaurav.bk@gmail.com>
2020-08-30 15:46:55 -0400

    Initial commit

---
+++ src/main/java/com/example/demo/Student.java
@@ -0,0 +1,64 @@
+package com.example.demo;
+
+import org.hibernate.annotations.GenericGenerator;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.util.StringJoiner;
+
+// This tells Hibernate to make a table out of this class
+@Entity
+public class Student {
+
+    @Id
+    @GeneratedValue(strategy=GenerationType.AUTO,generator="native")
+    @GenericGenerator(name = "native",strategy = "native")
+    private Integer id;
+    private String firstName;
+    private String lastName;
+    private String emailId;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public String getEmailId() {
+        return emailId;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public void setEmailId(String emailId) {
+        this.emailId = emailId;
+    }
+
+    @Override
+    public String toString() {
+        return new StringJoiner(", ", Student.class.getSimpleName() + "[", "]")
+                .add("id=" + id)
+                .add("firstName='" + firstName + "'")
+                .add("lastName='" + lastName + "'")
+                .add("emailId='" + emailId + "'")
+                .toString();
+    }
+}
